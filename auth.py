099e9d78458dbeaf19b7460af730a94dba9b4738
MatijaGH <nano.robot.electric@gmail.com>
2019-04-29 13:55:59 +0200

    V datoteki za generiranje podatkov napisal ukaze za ustvarjanje tabel in brisanje table. DOdal test, ki preverja ali v tabeli agent vse deluje v redu. Komentiral datoteko auth, da vemo v prihodnje, kaj narediti.

--- auth.py
+++ auth.py
@@ -1,4 +1,8 @@
-# Skopiraj datoteko v auth.py ali auth.R in vnesi podatke za priklop na bazo
+# Za uspešen test vneseš svoje podatke za priklop na bazo. Torej:
+#    db = sem2019_matijagh
+#    host = -=-
+#    user = matijagh
+#    geslo = ***
 db = 'sem2019_uporabnik'
 host = 'baza.fmf.uni-lj.si'
 user = 'uporabnik'
--- generiranje_podatkov.py
+++ generiranje_podatkov.py
@@ -1,10 +1,15 @@
 import auth
 import csv
 import pandas as pd
-auth.db = "sem2019_%s" % auth.user
+#auth.db = "sem2019_%s" % auth.user
+
 # uvozimo psycopg2
 import psycopg2, psycopg2.extensions, psycopg2.extras
+
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) # se znebimo problemov s šumniki
+
+
+#Preberemo lahko podatke, ki jih imamo
 
 agent = pd.read_csv('Podatki/agent.csv', encoding = "ISO-8859-1",
                    error_bad_lines=False,
@@ -38,7 +43,60 @@ sportni_direktor = pd.read_csv('Podatki/sportni_direktor.csv', encoding = "ISO-8
                    sep = ',')
 testsportni_direktor = list(sportni_direktor.ID)
 
+#USTVARJANJE TABEL
+#Kasnjeje bom malo spremenil zadeve, da bodo še foreign key v redu
 
+def ustvari_tabelo_agent():
+    cur.execute("""
+        CREATE TABLE agent (
+            id SERIAL PRIMARY KEY,
+            ime TEXT NOT NULL,
+            priimek TEXT NOT NULL,
+        );
+    """)
+conn.commit()
+
+def ustvari_tabelo_sportni_direktor():
+    cur.execute("""
+        CREATE TABLE sportni_direktor (
+            id SERIAL PRIMARY KEY,
+            ime TEXT NOT NULL,
+            priimek TEXTNOT NULL,
+        );
+    """)
+conn.commit()
+
+def ustvari_tabelo_klub():
+    cur.execute("""
+        CREATE TABLE klub (
+            id SERIAL PRIMARY KEY,
+            ime TEXT NOT NULL,
+            naslov TEXT NOT NULL,
+        );
+    """)
+conn.commit()
+
+#Ukazi za brisanje tabel
+
+def pobrisi_tabelo_agent():
+    cur.execute("""
+        DROP TABLE agent;
+    """)
+conn.commit()
+
+def pobrisi_tabelo_sportni_direktor():
+    cur.execute("""
+        DROP TABLE sportni_direktor;
+    """)
+conn.commit()
+
+def pobrisi_klub():
+    cur.execute("""
+        DROP TABLE klub;
+    """)
+conn.commit()
+
+#Ukazi za uvažanje podatkov
 
 def uvozi_podatke_agent():
     with open("Podatki/agent.csv") as f:
@@ -105,6 +163,13 @@ def uvozi_podatke_sportni_direktor():
             print("Uvožen sportni direktor %s z ID-jem %d" % (r[0], rid))
 conn.commit()
 
-conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password)
-cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
+#Test, ali vse deluje, kot mora
 
+def test():
+    cur.execute("select * from agent")
+    print(cur.fetchall())
+    
+conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password)
+conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
+test()
