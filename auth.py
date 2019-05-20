4686a3c1b4e51e3483392f14f382def5339d62ff
MatijaGH <31621502+MatijaGH@users.noreply.github.com>
2019-05-20 13:44:10 +0200

    Delete auth.py

--- auth.py
+++
@@ -1,9 +0,0 @@
-# Za uspešen test vneseš svoje podatke za priklop na bazo. Torej:
-db = 'sem2019_matijagh'
-host = 'baza.fmf.uni-lj.si'
-user = 'matijagh'
-password = 'f3wl64em'
-#db = 'sem2019_uporabnik'
-#host = 'baza.fmf.uni-lj.si'
-#user = 'uporabnik'
-#password = 'geslo'
