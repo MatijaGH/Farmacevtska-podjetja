7eeb1bba465448f8603b1c9a000a8818c0639bd3
oskarkregar <oskar.kregar@icloud.com>
2019-06-18 06:31:41 +0200

    Popravil, da dela login in nato cookie redirecti

--- glavni.py
+++ glavni.py
@@ -18,14 +18,19 @@ cur = baza.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
 ################
 #test priklopa na bazo(ni še v redu, popraviti moram program za tabelo)
-def test(ime):
+def test():
     cur.execute('''
-                    SELECT * FROM agent WHERE id=%s
-                ''', [ime])
+                    SELECT * FROM uporabnik WHERE vloga = 'agent'
+                    ''')
     return (cur.fetchall())
 
-print(test(3))
-
+print(test())
+def test2():
+    cur.execute('''
+                    SELECT * FROM uporabnik WHERE vloga = 'igralec'
+                    ''')
+    return (cur.fetchall())
+print(test2())
 ################
 #bottle uvod, pomozne funkcije
 
@@ -39,14 +44,13 @@ def vloga(user):
     return r
 
 
-def password_md5(s):
+#def password_md5(s):
     """Vrni MD5 hash danega UTF-8 niza. Gesla vedno spravimo v bazo
        kodirana s to funkcijo."""
-    h = hashlib.md5()
-    h.update(s.encode('utf-8'))
-    return h.hexdigest()
+    #p = hashlib.md5()
+    #p.update(s.encode('utf-8'))
+    #return p.hexdigest()
 
-print(password_md5("psimonds0"))
 
 def get_user(auto_login = True, auto_redir=False):
     """Poglej cookie in ugotovi, kdo je prijavljeni uporabnik,
@@ -54,15 +58,26 @@ def get_user(auto_login = True, auto_redir=False):
        na stran za prijavo ali vrni None (advisno od auto_login).
     """
     # Dobimo username iz piskotka
-    username = request.get_cookie('username', secret=secret)
+    username = request.get_cookie('username', secret=secret) 
+    print(username)
+    c = baza.cursor()
+    cur.execute('''
+                    SELECT * FROM uporabnik WHERE uporabnisko_ime=%s
+                    ''', [username])
+    tmp = cur.fetchone()
     # Preverimo, ali ta uporabnik obstaja
     if username is not None:
         #Ce uporabnik ze prijavljen, nima smisla, da je na route login
         if auto_redir:
-            redirect('/index/')
+                if tmp[2] == 'igralec':
+                    redirect('/index-igralec/')
+                elif tmp[2] == 'agent':
+                    redirect("/index-agent/")
+                else:
+                    redirect("/index-klub/")
         else:
             c = baza.cursor()
-            c.execute("SELECT username FROM uporabnik WHERE username=%s",
+            c.execute("SELECT uporabnik FROM uporabnik WHERE uporabnisko_ime=%s",
 					  [username])
             r = c.fetchone()
             c.close ()
@@ -77,16 +92,12 @@ def get_user(auto_login = True, auto_redir=False):
 
 
 #tukaj bo potrebno narediti še strani, kamor želimo preusmerjati in pogledati, kaj točno so parametri
-##def preusmeri(parameter, pooblastilo):
-##    if parameter == "agent":
-##        redirect('/indexagent/')
-##    elif parameter == "sportni_direktor":
-##        redirect('/indexsportni_direktor/')
-##    elif parameter == 'raziskovalec':
-##        if pooblastilo == 'zdravnik':
-##            redirect('/index/')
-##        elif pooblastilo == 'direktor':
-##    redirect('/indexdirektor/')
+def preusmeri(parameter, pooblastilo):
+    if parameter == "agent":
+        redirect('/index-agent/')
+    elif parameter == "igralec":
+        redirect('/index-igralec/')
+    
 
 
 @route("/static/<filename:path>")
@@ -106,66 +117,27 @@ def login_get():
                            napaka=None,
                            username=None)
 
-@get("/index-agent/")
-def index_get():
-    """Serviraj formo za index1."""
-    return template("index-agent.html")
-
-@get("/index-igralec/")
-def index_get():
-    """Serviraj formo za index1."""
-    return template("index-igralec.html")
-@get("/prestopi/")
-def index_get():
-    """Serviraj formo za index1."""
-    return template("prestopi.html")
-
-@get("/register/")
-def index_get():
-    """Serviraj formo za registracijo"""
-    return template("register.html")
-
-@get("/forget-pass/")
-def index_get():
-    """Serviraj formo za registracijo"""
-    return template("forget-pass.html")
-
-@get("/form/")
-def form_get():
-    """Serviraj formo za form"""
-    return template("form.html")
-
 @post('/login/', method='post')
 def do_login():
     """Obdelaj izpolnjeno formo za prijavo"""
     # Uporabnisko ime, ki ga je uporabnik vpisal v formo
     username = request.forms.get('username')
-    # Izracunamo MD5 has gesla, ki ga bomo spravili
-    password = password_md5(request.forms.get('password'))
+    # Spravimo geslo v bazo
+    password = request.forms.get('password')
+    print(username)
+    print(password)
     # Preverimo, ali se je uporabnik pravilno prijavil
     c = baza.cursor()
-    c.execute("SELECT * FROM uporabnik WHERE username=%s AND hash=%s",
-              [username, password])
-    tmp = c.fetchone()
-    # preverimo, ali je zahtevek mogoce v cakanju
-    c2 = baza.cursor()
-    c2.execute("SELECT * FROM zahtevek WHERE username=%s AND hash=%s",
-              [username, password])
-    tmp2 = c2.fetchone()
-    print(tmp2)
+    cur.execute('''
+                    SELECT * FROM uporabnik WHERE uporabnisko_ime=%s AND geslo=%s
+                    ''', [username, password])
+    tmp = cur.fetchone()
+    # preverimo, če je uporabnik v bazi
     if tmp is None:
-        if tmp2 is None:
             return template("login.html",
-                                   napaka="Nepravilna prijava",
-                                   username=username)
-        elif tmp2[7] == True:
-            return template("login.html",
-                            napaka="Nepravilna prijava",
-                            username=username)
-        else:
-            return template("login.html",
-                            napaka="Zahtevek registracije je v cakanju.",
-                            username=username)
+                            napaka="Nepravilna prijava.",
+                            username=None
+                     )
     else:
         response.set_cookie('username', username, path='/', secret=secret)
         if tmp[2] == 'igralec':
@@ -173,13 +145,87 @@ def do_login():
         elif tmp[2] == 'agent':
             redirect("/index-agent/")
         else:
-            redirect("/index2/")
-
-
+            redirect("/index-klub/")
 # else:
-# Vse je v redu, nastavimo cookie in preusmerimo na glavno stran
-#     response.set_cookie('username', username, path='/', secret=secret)
-#     redirect("/index/")
+    #     # Vse je v redu, nastavimo cookie in preusmerimo na glavno stran
+    #     response.set_cookie('username', username, path='/', secret=secret)
+    #     redirect("/index/")
 
 
-run(host='localhost', port=8080, debug = True, reloader = True)
+
+@get("/index-agent/")
+def index_agent_get():
+    """Serviraj formo za index1."""
+    return template("index-agent.html")
+
+@get("/index-igralec/")
+def index_igralec_get():
+    """Serviraj formo za index1."""
+    return template("index-igralec.html")
+@get("/prestopi/")
+def prestopi_get():
+    """Serviraj formo za index1."""
+    return template("prestopi.html")
+
+@get("/register/")
+def register_get():
+    """Serviraj formo za registracijo"""
+    curuser = get_user(auto_login = False, auto_redir = True)
+    return template("register.html")
+@post("/register/")
+def nov_zahtevek():
+    username = request.forms.username
+    ime = request.forms.ime
+    priimek = request.forms.priimek
+    select = request.form.get('select')
+    vloga= str(select)
+    email = request.forms.email
+
+    password = request.forms.password
+    password2 = request.forms.password2
+
+
+    c1 = baza.cursor()
+    c1.execute("SELECT * FROM uporabnik WHERE username=%s",
+              [username])
+    tmp = c1.fetchone()
+    if tmp is not None:
+        return template("register.html", ime=ime,
+                        priimek=priimek, vloga=vloga, email=email, napaka="Uporabniško ime je že zavzeto, izberi novega.")
+
+    #preverimo, ali se gesli ujemata
+    if password != password2:
+        return template("register.html", ime=ime,
+                        priimek=priimek, vloga=vloga, email=email, napaka="Gesli se ne ujemata!")
+
+
+    #ce pridemo, do sem, je vse uredu in lahko vnesemo zahtevek v bazo
+    c = baza.cursor()
+    c.execute("""INSERT INTO uporabniki (username, ime, priimek, vloga, mail, hash)
+                VALUES (%s, %s, %s, %s, %s, %s)""",
+              [username, ime, priimek, vloga, mail, password])
+    return template("register.html", name = None,
+                    surname=None, institution=None, mail=None, napaka="Prošnja poslana uspešno!")
+
+@get("/logout/")
+def logout():
+    """Pobrisi cookie in preusmeri na login."""
+    response.delete_cookie('username', path='/', secret=secret)
+    #print(get_user())
+    redirect('/login/')
+
+@get("/forget-pass/")
+def forget_pass_get():
+    """Serviraj formo za registracijo"""
+    curuser = get_user(auto_login = False, auto_redir = True)
+    return template("forget-pass.html")
+
+@get("/form/")
+def form_get():
+    """Serviraj formo za form"""
+    return template("form.html")
+
+
+
+
+run(host='localhost', port=8080)
