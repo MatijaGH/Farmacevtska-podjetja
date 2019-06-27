a4c754593befdb985577b7805b06aaed74b5b03c
Matija Gubanec Hančič <nano.robot.electric@gmail.com>
2019-06-27 18:10:13 +0200

    Urejena registracija, da deluje.

--- glavni.py
+++ glavni.py
@@ -233,54 +233,115 @@ def index_igralec_get():
 
 @get("/index-klub/")
 def index_klub_get():
-    """Serviraj formo za index-klub."""
+    """Serviraj formo za index-klub.html"""
     return template("index-klub.html")
 
 @get("/prestopi/")
 def prestopi_get():
-    """Serviraj formo za index1."""
+    """Serviraj formo za prestopi.html"""
     return template("prestopi.html")
 
 @get("/register/")
 def register_get():
     """Serviraj formo za registracijo"""
     curuser = get_user(auto_login = False, auto_redir = True)
-    return template("register.html")
+    return template("register.html", username = None, ime = None,
+                    priimek = None, vloga = None, email = None,
+                    DatumRojstva = None, geslo = None, geslo2 = None,
+                    naslov = None, država = None, napaka = None)
 
-@post("/register/")
+@post("/register/", method = 'post')
 def nov_zahtevek():
-    username = request.forms.username
-    ime = request.forms.ime
-    priimek = request.forms.priimek
-    select = request.form.get('select')
-    vloga= str(select)
-    email = request.forms.email
 
-    password = request.forms.password
-    password2 = request.forms.password2
+    #Za agenta
+    username = request.forms.get('username')
+    ime = request.forms.get('ime')
+    priimek = request.forms.get('priimek')
+    vloga= request.forms.get('vloga')
+    email = request.forms.get('email')
+    DatumRojstva = request.forms.get('DatumRojstva')
+    naslov = request.forms.get('naslov')
+    država = request.forms.get('država')
 
+    geslo = request.forms.get('geslo')
+    geslo2 = request.forms.get('geslo2')
 
+    #Za klub
+    username_klub = request.forms.get('username_klub')
+    ime_klub = request.forms.get('ime_klub')
+    naslov_klub = request.forms.get('naslov_klub')
+    print(vloga)
+    print(username_klub)
+    print(ime_klub)
+    print(geslo)
+    print(geslo2)
+
+    
     c1 = baza.cursor()
-    c1.execute("SELECT * FROM uporabnik WHERE username=%s",
+    c1.execute("SELECT * FROM uporabnik WHERE uporabnisko_ime=%s",
               [username])
     tmp = c1.fetchone()
+        
     if tmp is not None:
-        return template("register.html", ime=ime,
-                        priimek=priimek, vloga=vloga, email=email, napaka="Uporabniško ime je že zavzeto, izberi novega.")
+        return template("register.html", username = username, ime = ime,
+                    priimek = priimek, vloga = vloga, email = email,
+                    DatumRojstva = DatumRojstva, geslo = geslo, geslo2 = geslo2,
+                    naslov = naslov, država = država, napaka="Uporabniško ime je že zavzeto, izberi novega.")
 
     #preverimo, ali se gesli ujemata
-    if password != password2:
-        return template("register.html", ime=ime,
-                        priimek=priimek, vloga=vloga, email=email, napaka="Gesli se ne ujemata!")
+    if geslo != geslo2:
+        return template("register.html", username = username, ime = ime,
+                    priimek = priimek, vloga = vloga, email = email,
+                    DatumRojstva = DatumRojstva, geslo = geslo, geslo2 = geslo2,
+                    naslov = naslov, država = država, napaka="Gesli se ne ujemata!")
 
 
     #ce pridemo, do sem, je vse uredu in lahko vnesemo zahtevek v bazo
+
+    #pogledam največji id, da bom dodajal za tem
+    c1.execute("SELECT id FROM uporabnik ORDER BY id DESC LIMIT 1")
+    dolzina = c1.fetchone()[0]
+    id = dolzina + 1
     c = baza.cursor()
-    c.execute("""INSERT INTO uporabniki (username, ime, priimek, vloga, mail, hash)
-                VALUES (%s, %s, %s, %s, %s, %s)""",
-              [username, ime, priimek, vloga, mail, password])
-    return template("register.html", name = None,
-                    surname=None, institution=None, mail=None, napaka="Prošnja poslana uspešno!")
+    #vnesem agenta v bazo
+    if vloga == '1':
+        c.execute("""INSERT INTO uporabnik (id, uporabnisko_ime, geslo, vloga)
+                VALUES (%s, %s, %s, %s)""",
+              [id, username, geslo, 'agent;'])
+        c.execute("""INSERT INTO agent (id, ime, priimek)
+                VALUES (%s, %s, %s) """,
+                  [id, ime, priimek])
+        return template("register.html", username = None, ime = None,
+                    priimek = None, vloga = None, email = None,
+                    starost = None, geslo = None, geslo2 = None,
+                    naslov = None, napaka="Prošnja poslana uspešno!")
+    #vnesem igralca v bazo
+    elif vloga == '2':
+        c.execute("""INSERT INTO uporabnik (id, uporabnisko_ime, geslo, vloga)
+                VALUES (%s, %s, %s, %s)""",
+              [id, username, geslo, 'igralec;'])
+        c.execute("""INSERT INTO igralci (id, ime, priimek, država, plača, datum_rojstva, vrednost, klub, agent)
+                VALUES (%s, %s, %s, %s, %s, %s, %s) """,
+                  [id, ime, priimek, država, '0', DatumRojstva, '0', None, None])
+        print("Uspeh!")
+        return template("register.html", username = None, ime = None,
+                    priimek = None, vloga = None, email = None,
+                    starost = None, geslo = None, geslo2 = None,
+                    naslov = None, napaka="Prošnja poslana uspešno!")
+    #vnesem klub v bazo
+    elif vloga == '3':
+        print('Gasa')
+        c.execute("""INSERT INTO uporabnik (id, uporabnisko_ime, geslo, vloga)
+                VALUES (%s, %s, %s, %s)""",
+              [id, username, geslo, 'klub;'])
+        c.execute("""INSERT INTO klub (id, ime, naslov)
+                VALUES (%s, %s, %s) """,
+                  [id, ime, naslov])
+        print("Uspeh!")
+        return template("register.html", username = None, ime = None,
+                    priimek = None, vloga = None, email = None,
+                    starost = None, geslo = None, geslo2 = None,
+                    naslov = None, napaka="Prošnja poslana uspešno!")
 
 @get("/logout/")
 def logout():
@@ -291,7 +352,7 @@ def logout():
 
 @get("/forget-pass/")
 def forget_pass_get():
-    """Serviraj formo za registracijo"""
+    """Serviraj formo za pozabjeno geslo"""
     curuser = get_user(auto_login = False, auto_redir = True)
     return template("forget-pass.html")
 
